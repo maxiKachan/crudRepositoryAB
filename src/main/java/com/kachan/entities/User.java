ae4a7e58860e99c0d393618b3a93da514b2e8451
maxiKachan <max101111427@gmail.com>
2022-02-16 11:48:22 +0300

    Added persistence annotations to entity User

--- src/main/java/com/kachan/entities/User.java
+++ src/main/java/com/kachan/entities/User.java
@@ -1,20 +1,23 @@
 package com.kachan.entities;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Table(name = "a_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private long id;
+    @Column(name = "surname")
     private String surname;
+    @Column(name = "first_name")
     private String firstname;
+    @Column(name = "last_name")
     private String lastname;
+    @Column(name = "phone_number")
     private String phoneNumber;
 
     public User() {
